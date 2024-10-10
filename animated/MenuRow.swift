a91b24f2decbcd30d55b3d3daaf2e959074ea25c
HanlopDev <hanafbomar@gmail.com>
2024-10-10 09:57:33 +0300

    Implement ContenView Animations

---
+++ animated/MenuRow.swift
@@ -0,0 +1,18 @@
+//
+//  MenuRow.swift
+//  animated
+//
+//  Created by Mpore on 10/10/2024.
+//
+
+import SwiftUI
+
+struct MenuRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuRow()
+}
