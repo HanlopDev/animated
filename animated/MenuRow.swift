bc89e0ce52ec2184604f7c13cdfa9b29266a3134
HanlopDev <hanafbomar@gmail.com>
2024-10-17 14:59:37 +0300

    implement final  menubar style

--- animated/MenuRow.swift
+++ animated/MenuRow.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct MenuRow: View {
+    var item: MenuItem
+    @Binding var selectedMenu: SelectedMenu
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 14) {
+            item.icon.view()
+                .frame(width: 32, height: 32)
+                .opacity(0.6)
+            Text(item.text)
+                .customFont(.headline)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(12)
+        .background(
+            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                .fill(.blue)
+                .frame(maxWidth: selectedMenu == item.menu ? .infinity : 0)
+                .frame(maxWidth: .infinity, alignment: .leading)
+        )
+        .background(Color("Background 2"))
+        .onTapGesture {
+            try? item.icon.setInput("active", value: true)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+                try? item.icon.setInput("active", value: false)
+            }
+            withAnimation(.timingCurve(0.2,0.8,0.2,1)){
+                selectedMenu = item.menu
+            }
+        }
     }
 }
 
 #Preview {
-    MenuRow()
+    MenuRow(item: menuItems[0], selectedMenu: .constant(.home))
 }
--- animated/animatedApp.swift
+++ animated/animatedApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct animatedApp: App {
     var body: some Scene {
         WindowGroup {
-            OnboardingView()
+            ContentView()
         }
     }
 }
